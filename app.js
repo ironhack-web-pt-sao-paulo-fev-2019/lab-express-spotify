0136d8b385666370352c965523aef514f4ba8bd9
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-04-04 20:12:49 -0300

    Terminado Step 2

--- app.js
+++ app.js
@@ -40,6 +40,22 @@ app.get('/', (request, response) => {
     response.render('index');
 });
 
+app.get('/artists', (request,response) => {
+    
+    const artistName = request.query.search; 
+    spotifyApi.searchArtists(artistName)
+    .then(data => {
+
+      console.log("The received data from the API: ", data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      const artistData = data.body.artists.items;
+      response.render('artists', {artistData});
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+
+})
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
